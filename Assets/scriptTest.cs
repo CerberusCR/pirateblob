5fd18196fc9188819206beaf9e72660d64e361b9
CerberusCR <124683059+CerberusCR@users.noreply.github.com>
2023-09-12 22:56:14 +0200

    testeroony

--- Assets/scriptTest.cs
+++ Assets/scriptTest.cs
@@ -7,7 +7,7 @@ public class scriptTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /// hello world
     }
 
     // Update is called once per frame
