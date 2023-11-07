98da5c8dc918204ab4a424e6192fda54ff53d7cc
Luukiep <97096896@st.deltion.nl>
2023-11-07 16:58:07 +0100

    Fixed the Lanturns let's gooooooooo

--- Assets/Code/TowerAi.cs
+++ Assets/Code/TowerAi.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class TowerAi : MonoBehaviour
 {
     public List<Transform> targets = new List<Transform>(); // List of target objects' Transforms
+    public GameObject launchableObject; // Reference to the existing launchable object
     public float range = 10.0f; // Range within which the follower object follows a target
     public float rotationSpeed = 5.0f; // Speed of the smooth transition
+    public float launchSpeed = 10.0f; // Speed to launch the object
+    public float respawnDelay = 2.0f; // Time in seconds to respawn the launchable object
     private int currentTargetIndex = 0; // Index of the current target
     private Animator animator;
+    private Vector3 launchDirection;
+
+    private Vector3 initialLaunchableObjectPosition;
+    private Quaternion initialLaunchableObjectRotation;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        initialLaunchableObjectPosition = launchableObject.transform.position;
+        initialLaunchableObjectRotation = launchableObject.transform.rotation;
     }
 
     void Update()
@@ -39,6 +48,12 @@ public class TowerAi : MonoBehaviour
 
                 // Play the animation
                 animator.SetBool("IsInRange", true);
+
+                // Calculate the launch direction
+                launchDirection = transform.forward;
+
+                // Launch the object when the target is in range
+                LaunchObject();
             }
             else
             {
@@ -72,5 +87,31 @@ public class TowerAi : MonoBehaviour
             }
         }
         return false;
+    }
+
+    void LaunchObject()
+    {
+        if (launchableObject != null)
+        {
+            // Move the object using translation
+            launchableObject.transform.Translate(launchDirection * launchSpeed * Time.deltaTime);
+        }
+    }
+
+    IEnumerator RespawnLaunchableObject()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+
+        // Reset the launchable object's position and rotation
+        launchableObject.transform.position = initialLaunchableObjectPosition;
+        launchableObject.transform.rotation = initialLaunchableObjectRotation;
+
+        // Re-enable the launchable object and its Collider
+        launchableObject.SetActive(true);
+        Collider launchableObjectCollider = launchableObject.GetComponent<Collider>();
+        if (launchableObjectCollider != null)
+        {
+            launchableObjectCollider.enabled = true;
+        }
     }
 }
