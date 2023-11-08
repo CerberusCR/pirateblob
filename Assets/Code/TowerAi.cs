639d1290f44138e31c24574c963ab5a916798fc7
Luukiep <97096896@st.deltion.nl>
2023-11-08 14:37:02 +0100

    preparing for presitation

--- Assets/Code/TowerAi.cs
+++ Assets/Code/TowerAi.cs
@@ -5,23 +5,13 @@ using UnityEngine;
 public class TowerAi : MonoBehaviour
 {
     public List<Transform> targets = new List<Transform>(); // List of target objects' Transforms
-    public GameObject launchableObject; // Reference to the existing launchable object
     public float range = 10.0f; // Range within which the follower object follows a target
     public float rotationSpeed = 5.0f; // Speed of the smooth transition
-    public float launchSpeed = 10.0f; // Speed to launch the object
-    public float respawnDelay = 2.0f; // Time in seconds to respawn the launchable object
-    private int currentTargetIndex = 0; // Index of the current target
     private Animator animator;
-    private Vector3 launchDirection;
-
-    private Vector3 initialLaunchableObjectPosition;
-    private Quaternion initialLaunchableObjectRotation;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        initialLaunchableObjectPosition = launchableObject.transform.position;
-        initialLaunchableObjectRotation = launchableObject.transform.rotation;
     }
 
     void Update()
@@ -29,37 +19,23 @@ public class TowerAi : MonoBehaviour
         if (targets.Count == 0)
             return; // No targets to follow
 
-        if (currentTargetIndex >= targets.Count)
-            currentTargetIndex = 0; // Reset to the first target if we've reached the end of the list
-
-        Transform currentTarget = targets[currentTargetIndex];
+        Transform currentTarget = GetNearestInRangeTarget();
 
         if (currentTarget != null)
         {
-            float distanceToTarget = Vector3.Distance(transform.position, currentTarget.position);
+            // Calculate the target rotation
+            Quaternion targetRotation = Quaternion.LookRotation(currentTarget.position - transform.position);
 
-            if (distanceToTarget <= range)
-            {
-                // Calculate the target rotation
-                Quaternion targetRotation = Quaternion.LookRotation(currentTarget.position - transform.position);
+            // Smoothly interpolate the rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-                // Smoothly interpolate the rotation
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-                // Play the animation
-                animator.SetBool("IsInRange", true);
-
-                // Calculate the launch direction
-                launchDirection = transform.forward;
-
-                // Launch the object when the target is in range
-                LaunchObject();
-            }
-            else
-            {
-                // Stop the animation
-                animator.SetBool("IsInRange", false);
-            }
+            // Play the animation
+            animator.SetBool("IsInRange", true);
+        }
+        else
+        {
+            // Stop the animation
+            animator.SetBool("IsInRange", false);
         }
 
         // Disable the Animator when no target is in range
@@ -71,6 +47,28 @@ public class TowerAi : MonoBehaviour
         {
             animator.enabled = true;
         }
+    }
+
+    Transform GetNearestInRangeTarget()
+    {
+        Transform nearestTarget = null;
+        float nearestDistance = range + 1.0f; // Set to a value greater than the range
+
+        foreach (Transform target in targets)
+        {
+            if (target != null)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (distanceToTarget <= range && distanceToTarget < nearestDistance)
+                {
+                    nearestDistance = distanceToTarget;
+                    nearestTarget = target;
+                }
+            }
+        }
+
+        return nearestTarget;
     }
 
     bool AnyTargetInRange()
@@ -87,31 +85,5 @@ public class TowerAi : MonoBehaviour
             }
         }
         return false;
-    }
-
-    void LaunchObject()
-    {
-        if (launchableObject != null)
-        {
-            // Move the object using translation
-            launchableObject.transform.Translate(launchDirection * launchSpeed * Time.deltaTime);
-        }
-    }
-
-    IEnumerator RespawnLaunchableObject()
-    {
-        yield return new WaitForSeconds(respawnDelay);
-
-        // Reset the launchable object's position and rotation
-        launchableObject.transform.position = initialLaunchableObjectPosition;
-        launchableObject.transform.rotation = initialLaunchableObjectRotation;
-
-        // Re-enable the launchable object and its Collider
-        launchableObject.SetActive(true);
-        Collider launchableObjectCollider = launchableObject.GetComponent<Collider>();
-        if (launchableObjectCollider != null)
-        {
-            launchableObjectCollider.enabled = true;
-        }
     }
 }
